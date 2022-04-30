2ac02df519dcd963ab50810edd9152c02edbe366
Mike Breault <mbreault@users.noreply.github.com>
2022-04-30 12:49:52 -0500

    checking for overflow example

---
+++ Chapter03-vscode/CheckingForOverflow/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+try
+{
+    int x = int.MaxValue - 1;
+    WriteLine($"Initial Value: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
+}
+catch (OverflowException) {
+    WriteLine("Caught an overflow exception.");
+}
