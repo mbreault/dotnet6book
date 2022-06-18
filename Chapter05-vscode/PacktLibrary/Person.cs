f6823af09bcc671905c02ce14aaf38b63b741ad9
Mike Breault <mbreault@users.noreply.github.com>
2022-06-18 13:24:00 -0500

    constructors

--- Chapter05-vscode/PacktLibrary/Person.cs
+++ Chapter05-vscode/PacktLibrary/Person.cs
@@ -5,6 +5,20 @@ namespace Packt.Shared
 {
     public class Person
     {
+        public Person()
+        {
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
+        public readonly DateTime Instantiated;
         public const string Species = "Homo Sapiens";
         public readonly string HomePlanet = "Earth";
         public string Name;
