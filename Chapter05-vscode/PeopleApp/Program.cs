bfdc9e49872ed820cb346a96f3fd3247d7fd7fbd
Mike Breault <mbreault@users.noreply.github.com>
2022-06-24 22:46:18 -0500

    pattern matching

--- Chapter05-vscode/PeopleApp/Program.cs
+++ Chapter05-vscode/PeopleApp/Program.cs
@@ -52,7 +52,7 @@ WriteLine(format:
   arg2: blankPerson.Instantiated
   );
 
-  Person gunny = new(initialName: "Gunny",homePlanet: "Mars");
+Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
 WriteLine(format:
   "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
   arg0: gunny.Name,
@@ -60,7 +60,7 @@ WriteLine(format:
   arg2: gunny.Instantiated
   );
 
-bob.WriteToConsole(); 
+bob.WriteToConsole();
 WriteLine(bob.GetOrigin());
 
 (string, int) fruit = bob.GetFruit();
@@ -68,7 +68,7 @@ WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
 
 var thing1 = ("Neville", 4);
 WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
-var thing2 = (bob.Name, bob.Children.Count); 
+var thing2 = (bob.Name, bob.Children.Count);
 WriteLine($"{thing2.Name} has {thing2.Count} children.");
 
 // Deconstructed tuple
@@ -81,44 +81,66 @@ WriteLine($"Deconstructed: {name1}, {dob1}");
 var (name2, dob2, fav2) = bob;
 WriteLine($"Deconstructed: {name2}, {dob2}, {fav2}");
 
-WriteLine(bob.SayHello()); 
+WriteLine(bob.SayHello());
 WriteLine(bob.SayHelloTo("Emily"));
 WriteLine(bob.OptionalParameters());
 WriteLine(bob.OptionalParameters("Jump1", 98.5));
 WriteLine(bob.OptionalParameters(number: 52.7, command: "Hide!"));
 WriteLine(bob.OptionalParameters("Poke!", active: false));
 
-int a = 10; 
-int b = 20; 
+int a = 10;
+int b = 20;
 int c = 30;
-WriteLine($"Before: a = {a}, b = {b}, c = {c}"); 
-bob.PassingParameters(a, ref b, out c); 
+WriteLine($"Before: a = {a}, b = {b}, c = {c}");
+bob.PassingParameters(a, ref b, out c);
 WriteLine($"After: a = {a}, b = {b}, c = {c}");
 
-int d = 10; 
+int d = 10;
 int e = 20;
 WriteLine($"Before: d = {d}, e = {e}, f doesn't exist yet!");
 // simplified C# 7.0 or later syntax for the out parameter 
-bob.PassingParameters(d, ref e, out int f); 
+bob.PassingParameters(d, ref e, out int f);
 WriteLine($"After: d = {d}, e = {e}, f = {f}");
 
 Person sam = new()
 {
-  Name = "Sam",
-  DateOfBirth = new(1972, 1, 27)
+    Name = "Sam",
+    DateOfBirth = new(1972, 1, 27)
 };
-WriteLine(sam.Origin); 
-WriteLine(sam.Greeting); 
+WriteLine(sam.Origin);
+WriteLine(sam.Greeting);
 WriteLine(sam.Age);
 
 sam.FavoriteIceCream = "Chocolate Fudge";
-WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}."); 
+WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}.");
 sam.FavoritePrimaryColor = "Red";
 WriteLine($"Sam's favorite primary color is {sam.FavoritePrimaryColor}.");
 
-sam.Children.Add(new() { Name = "Charlie" }); 
+sam.Children.Add(new() { Name = "Charlie" });
 sam.Children.Add(new() { Name = "Ella" });
-WriteLine($"Sam's first child is {sam.Children[0].Name}"); 
+WriteLine($"Sam's first child is {sam.Children[0].Name}");
 WriteLine($"Sam's second child is {sam.Children[1].Name}");
-WriteLine($"Sam's first child is {sam[0].Name}"); 
+WriteLine($"Sam's first child is {sam[0].Name}");
 WriteLine($"Sam's second child is {sam[1].Name}");
+
+object[] passengers = {
+  new FirstClassPassenger { AirMiles = 1_419 },
+  new FirstClassPassenger { AirMiles = 16_562 },
+  new BusinessClassPassenger(),
+  new CoachClassPassenger { CarryOnKG = 25.7 },
+  new CoachClassPassenger { CarryOnKG = 0 },
+};
+foreach (object passenger in passengers)
+{
+    decimal flightCost = passenger switch
+    {
+        FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+        FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+        FirstClassPassenger _ => 2000M,
+        BusinessClassPassenger _ => 1000M,
+        CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+        CoachClassPassenger _ => 650M,
+        _ => 800M
+    };
+    WriteLine($"Flight costs {flightCost:C} for {passenger}");
+}
