d6a1cb70bed9d5945227d3da6e3974194f19a3e4
Mike Breault <mbreault@users.noreply.github.com>
2022-06-25 09:15:38 -0500

    records

---
+++ Chapter05-vscode/PacktLibraryModern/Record.cs
@@ -0,0 +1,15 @@
+namespace Packt.Shared; // C# 10 file-scoped namespace
+public class ImmutablePerson
+{
+  public string? FirstName { get; init; }
+  public string? LastName { get; init; }
+}
+
+public record ImmutableVehicle
+{
+  public int Wheels { get; init; }
+  public string? Color { get; init; }
+  public string? Brand { get; init; }
+}
+
+public record ImmutableAnimal(string Name, string Species);
