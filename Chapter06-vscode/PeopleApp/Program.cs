addbfb95c4f02d9c773408633b370221c79b699b
Mike Breault <mbreault@users.noreply.github.com>
2022-06-27 12:10:39 -0500

    operators

--- Chapter06-vscode/PeopleApp/Program.cs
+++ Chapter06-vscode/PeopleApp/Program.cs
@@ -9,6 +9,8 @@ Person baby1 = mary.ProcreateWith(harry);
 baby1.Name = "Gary";
 // call static method
 Person baby2 = Person.Procreate(harry, jill);
+// call an operator
+Person baby3 = harry * mary;
 WriteLine($"{harry.Name} has {harry.Children.Count} children."); 
 WriteLine($"{mary.Name} has {mary.Children.Count} children."); 
 WriteLine($"{jill.Name} has {jill.Children.Count} children."); 
@@ -16,3 +18,8 @@ WriteLine(
   format: "{0}'s first child is named \"{1}\".",
   arg0: harry.Name,
   arg1: harry.Children[0].Name);
+
+
+
+
+
