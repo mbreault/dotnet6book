2c4fba6f684f3432078fe172aa2ff5053a1e49be
Mike Breault <mbreault@users.noreply.github.com>
2022-07-05 05:59:30 -0500

    null reference types

---
+++ Chapter06-vscode/NullHandling/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+int thisCannotBeNull  = 4; 
+//thisCannotBeNull = null; // compile error!
+int? thisCouldBeNull = null; 
+WriteLine(thisCouldBeNull); 
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+thisCouldBeNull = 7; 
+WriteLine(thisCouldBeNull); 
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+
+Address address = new();
+address.Building = null;
+address.Street = null;
+address.City = "London";
+address.Region = null;
