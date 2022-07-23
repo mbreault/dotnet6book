c83178e617e007235bbeea4509edb6ca0de59d84
Mike Breault <mbreault@users.noreply.github.com>
2022-07-22 19:41:40 -0500

    wpf

---
+++ Chapter12-vscode/SynchronizingResourceAccess/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using static System.Console;
+
+WriteLine("Please wait for the tasks to complete.");
+Stopwatch watch = Stopwatch.StartNew();
+Task a = Task.Factory.StartNew(MethodA);
+Task b = Task.Factory.StartNew(MethodB);
+ 
+Task.WaitAll(new Task[] { a, b });
+
+WriteLine();
+WriteLine($"Results: {SharedObjects.Message}.");
+WriteLine($"{watch.ElapsedMilliseconds:N0} elapsed milliseconds.");
+
+static void MethodA()
+{
+    lock (SharedObjects.Conch)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread.Sleep(SharedObjects.Random.Next(2000));
+            SharedObjects.Message += "A";
+            Write(".");
+        }
+    }
+}
+static void MethodB()
+{
+    lock (SharedObjects.Conch)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread.Sleep(SharedObjects.Random.Next(2000));
+            SharedObjects.Message += "B";
+            Write(".");
+        }
+    }
+}
+static class SharedObjects
+{
+  public static object Conch = new();
+  public static Random Random = new();
+  public static string? Message; // a shared resource
+}
+
+
